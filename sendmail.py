38668e95fd3054835c5f6f809e49a8ac20a3fe1b
pratyakshchauhan <122276706+pratyakshchauhan@users.noreply.github.com>
2023-04-14 16:49:15 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -14,11 +14,11 @@ USER_EMAIL = os.environ.get("USER_EMAIL")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
 ISSUE = os.environ.get("ISSUE_TITLE")
 RECIVERS_MAIL = os.environ.get("RECIEVERS_MAIL")
-# RECIVERS_MAIL1 = os.environ.get("RECIEVERS_MAIL1")
+RECIVERS_MAIL1 = os.environ.get("RECIEVERS_MAIL1")
 TEXT = os.environ.get("ISSUE_DESC")
 
 
-to_address = [RECIVERS_MAIL]
+to_address = [RECIVERS_MAIL, RECIVERS_MAIL1]
 actor = os.environ.get("ISSUE_CREATOR")
 for i in mail_list:
   if actor == i:
@@ -29,7 +29,7 @@ for i in mail_list:
 
 SUBJECT = "Issue: " + ISSUE
 
-body = "The following issue has been encountered: " + TEXT + " and has been assigned to " + actor +"."
+body = "The following issue has been encountered: " + TEXT + ", and has been assigned to " + actor +"."
 message = 'Subject: {}\n\n{}'.format(SUBJECT, body)
 context = ssl.create_default_context()
 
