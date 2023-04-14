55fbedec1a16fd9b3622b5dd50cb900ddca2cd40
pratyakshchauhan <122276706+pratyakshchauhan@users.noreply.github.com>
2023-04-14 13:14:13 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -14,4 +14,4 @@ context = ssl.create_default_context()
 server = smtplib.SMTP_SSL(smtp_server, port, context=context)
 
 server.login(USER_EMAIL, USER_PASSWORD)
-server.sendmail(RECIVERS_MAIL, USER_EMAIL, message)
+server.sendmail(RECIVERS_MAIL, RECIVERS_MAIL, message)
