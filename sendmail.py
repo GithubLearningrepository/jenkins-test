497b633df46886ba3f3e66944dde842f4d83555f
pratyakshchauhan <122276706+pratyakshchauhan@users.noreply.github.com>
2023-04-14 13:17:58 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -7,7 +7,8 @@ USER_EMAIL = os.environ.get("USER_EMAIL")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
 ISSUE = os.environ.get("ISSUE_TITLE")
 RECIVERS_MAIL = os.environ.get("RECIEVERS_MAIL")
-# to_addr = pratyaksh_chauhan@fosteringlinux.com
+RECIVERS_MAIL1 = os.environ.get("RECIEVERS_MAIL1")
+to_addr = [RECIVERS_MAIL, RECIVERS_MAIL1]
 message = ISSUE
 context = ssl.create_default_context()
 
