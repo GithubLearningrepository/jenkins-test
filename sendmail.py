0b61f5032776ce57ede36038ade8d811ace33215
pratyakshchauhan <122276706+pratyakshchauhan@users.noreply.github.com>
2023-04-14 12:25:55 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -5,13 +5,9 @@ port = 465
 smtp_server = "smtp.gmail.com"
 USER_EMAIL = os.environ.get("USER_EMAIL")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
+ISSUE = os.environ.get("ISSUE_TITLE")
 
-message = """\
-    Subject: Welcome Ubaydah
-
-    This is your welcome email running 
-"""
-
+message = ISSUE
 context = ssl.create_default_context()
 
 server = smtplib.SMTP_SSL(smtp_server, port, context=context)
