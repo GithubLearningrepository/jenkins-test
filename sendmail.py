c5669d2d1842faefac2e235fe60468abccd6cfb1
pratyakshchauhan <122276706+pratyakshchauhan@users.noreply.github.com>
2023-04-14 13:00:45 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -6,7 +6,8 @@ smtp_server = "smtp.gmail.com"
 USER_EMAIL = os.environ.get("USER_EMAIL")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
 ISSUE = os.environ.get("ISSUE_TITLE")
-
+RECIVERS_MAIL = OS.environ.get("RECIEVERS_MAIL")
+to_addr = [USER_EMAIL, RECIVERS_MAIL]
 message = ISSUE
 context = ssl.create_default_context()
 
