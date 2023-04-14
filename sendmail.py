ce45b6ae684aa10f694e34e3c0416f3ace457ff9
pratyakshchauhan <122276706+pratyakshchauhan@users.noreply.github.com>
2023-04-14 13:12:06 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -7,11 +7,11 @@ USER_EMAIL = os.environ.get("USER_EMAIL")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
 ISSUE = os.environ.get("ISSUE_TITLE")
 RECIVERS_MAIL = os.environ.get("RECIEVERS_MAIL")
-to_addr = pratyaksh_chauhan@fosteringlinux.com
+# to_addr = pratyaksh_chauhan@fosteringlinux.com
 message = ISSUE
 context = ssl.create_default_context()
 
 server = smtplib.SMTP_SSL(smtp_server, port, context=context)
 
 server.login(USER_EMAIL, USER_PASSWORD)
-server.sendmail(to_addr, USER_EMAIL, message)
+server.sendmail(RECIVERS_MAIL, USER_EMAIL, message)
